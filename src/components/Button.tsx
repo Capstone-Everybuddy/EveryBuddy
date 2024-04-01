84a8530c3e0dcd97a51e37872dec641019d5c71a
subin <200516bb@gmail.com>
2024-04-01 20:46:59 +0900

    feat: Button 컴포넌트 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return <ButtonWrapper onClick={onClick}>{text}</ButtonWrapper>;
+};
+
+const ButtonWrapper = styled.button`
+  background-color: ${(props) => props.theme.colors.orange};
+  border: none;
+  border-radius: 100px;
+  padding: 16px;
+  color: white;
+  font-weight: 700;
+  font-size: 20px;
+  cursor: pointer;
+  width: 100%;
+`;
+
+export default Button;
