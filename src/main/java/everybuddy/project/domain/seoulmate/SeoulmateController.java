6ec19a42849fbea0fef3721c8d9bd5d5d9be5ea4
Chloe <kyjjulia07@gmail.com>
2024-04-17 20:25:03 +0900

    refactor: edit swagger comment
    swagger 코멘트 수정

--- src/main/java/everybuddy/project/domain/seoulmate/SeoulmateController.java
+++ src/main/java/everybuddy/project/domain/seoulmate/SeoulmateController.java
@@ -25,6 +25,7 @@ public class SeoulmateController {
 
     @ResponseBody
     @PostMapping("/log-in")
+    @Operation(summary = "서울메이트 log-in API", description = "서울메이트 로그인 API")
     public BaseResponse<PostLoginRes> loginSeoulmate(PostLoginReq postLoginReq) {
         try {
             PostLoginRes postLoginRes = seoulmateService.loginSeoulmate(postLoginReq);
@@ -37,6 +38,8 @@ public class SeoulmateController {
 
     @ResponseBody
     @PostMapping("/preference/{seoulmateIdx}")
+    @Operation(summary = "서울메이트 선호도 입력 API", description = "서울메이트 선호도 입력 API" +
+            "first, firstList와 같이 선호 순위대로 입력을 받고, 해당하는 값들을 테이블에 저장합니다.")
     public BaseResponse<PostPreferRes> savePreference(@RequestBody PostPreferReq postPreferReq, @PathVariable int seoulmateIdx) {
         try {
             PostPreferRes postPreferRes = seoulmateService.savePreference(postPreferReq, seoulmateIdx);
@@ -48,6 +51,8 @@ public class SeoulmateController {
 
     @ResponseBody
     @PostMapping("/info/{seoulmateIdx}")
+    @Operation(summary = "서울메이트 자신의 정보 입력 API", description = "서울메이트 자신의 정보 입력 API" +
+            "language, personality, hobby, wanttodo 순서대로 입력을 받고, 해당하는 값들을 테이블에 저장합니다.")
     public BaseResponse<String> saveInfo(@RequestBody PostInfoReq postInfoReq, @PathVariable int seoulmateIdx) {
         try {
             seoulmateService.saveInfo(postInfoReq, seoulmateIdx);
