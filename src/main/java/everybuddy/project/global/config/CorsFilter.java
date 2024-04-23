758b58a93ce7531e3defc092573fd054ce61ff8b
haochaen73 <kyjjulia07@gmail.com>
2024-04-23 10:38:52 +0900

    feat: CORS

---
+++ src/main/java/everybuddy/project/global/config/CorsFilter.java
@@ -0,0 +1,4 @@
+package everybuddy.project.global.config;
+
+public class CorsFilter {
+}
---
+++ src/main/java/everybuddy/project/global/config/WebConfig.java
@@ -0,0 +1,53 @@
+package everybuddy.project.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import com.deepit.stock.filter.JwtAuthorizationFilter;
+//import com.deepit.stock.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    //private final JwtTokenProvider jwtTokenProvider;
+    //@Value("${jwt.filter.enable:false}")
+    //private boolean jwtFilterEnable;
+//    @Bean
+//    public FilterRegistrationBean<JwtAuthorizationFilter> jwtAuthorizationfilter() {
+//        FilterRegistrationBean<JwtAuthorizationFilter> filterBean = new FilterRegistrationBean<>();
+//        filterBean.setFilter(new JwtAuthorizationFilter(jwtTokenProvider, jwtFilterEnable));
+//        filterBean.addUrlPatterns("/*");
+//        filterBean.setOrder(0);
+//        return filterBean;
+//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false).maxAge(6000);
+    }
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(6000L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+        return filterBean;
+    }
+}
