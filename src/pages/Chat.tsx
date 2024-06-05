12ce5c9a2ed1da0c37076ba8f1109caa2cc42696
subin <200516bb@gmail.com>
2024-06-05 15:18:11 +0900

    feat: 언어, 국적 항목 업데이트

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,16 +3,29 @@ import ChatList from 'components/Chatting/ChatList/ChatList';
 import Header from 'components/Header';
 import Layout from 'components/Layout';
 import Main from 'components/Main';
+import useMatchingState from 'hooks/useMatchingState';
+import styled from 'styled-components';
 
 const Chatting = () => {
+  const { isMatchingComplete } = useMatchingState();
   return (
     <Main.Wrapper>
       <Layout.PageContent>
         <Header title="Chat" />
-        <ChatList />
+        {!isMatchingComplete ? (
+          <ChatList />
+        ) : (
+          <CompleteMessage>
+            매칭이 완료된 후 채팅방이 생성됩니다.
+          </CompleteMessage>
+        )}
       </Layout.PageContent>
     </Main.Wrapper>
   );
 };
+
+const CompleteMessage = styled.div`
+  padding: 10px 0px;
+`;
 
 export default Chatting;
