e6e35eff29231f14e7d3b204191544a9775d93be
Chloe <kyjjulia07@gmail.com>
2024-06-06 12:47:54 +0900

    feat: WebSocketConfig

---
+++ src/main/java/everybuddy/project/global/config/WebSocketConfig.java
@@ -0,0 +1,14 @@
+package everybuddy.project.global.config;
+
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
